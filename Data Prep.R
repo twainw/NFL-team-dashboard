7ddbdc71f820aa5cf683eac0b49f311690c29d9e
twainw <patelivan74@gmail.com>
2023-07-09 13:30:41 -0700

    A lot of work to do

--- Data Prep.R
+++ Data Prep.R
@@ -9,6 +9,7 @@
 library(tidyverse)
 library(nflreadr)
 library(rlang)
+library(nflfastR)
 
 #--------------------------
 # Data
@@ -18,29 +19,24 @@ season_yr <- 2022
 pbp <- load_pbp(seasons = season_yr)
 schedule <- load_schedules(seasons = season_yr)
 
+schedule |> 
+  clean_homeaway() |> 
+  View()
+
 #--------------------------------------------------------
           # Data for team preview page visuals #
 #--------------------------------------------------------
 
 #--------------------------
-# Function to calculate
-# offense and defense stats
+# Advanced Stats and Rank Summary
 #--------------------------
 
 calculate_adv_stats <- function(team_var, type_var, pts_var){
   
-  # Step: Build pts_df
-  pts_df <- schedule |> 
-    clean_homeaway() |>
-    mutate(season_type = ifelse(game_type == "REG", "REG", "POST")) |> 
-    group_by(season, season_type, team) |> 
-    summarize(total_pts = sum({{pts_var}})) |> 
-    ungroup()
-  
   # Step: Build and return advanced stats df
-  pbp |> 
+  stats_df <- pbp |> 
     filter(!is.na(epa), !is.na({{team_var}}), pass == 1 | rush == 1) |> 
-    group_by(season, season_type, team = {{team_var}}, type = type_var) |>
+    group_by(season, week, season_type, team = {{team_var}}, type = type_var) |>
     summarize(total_plays = n()
               , total_epa = sum(epa)
               , total_success = sum(success)
@@ -51,20 +47,32 @@ calculate_adv_stats <- function(team_var, type_var, pts_var){
               , total_rush_epa = sum(epa*rush)
               , total_rush_success = sum(success*rush)) |> 
     ungroup() |> 
-    left_join(pts_df
-              , by = c("season", "season_type", "team")) %>%
-    return()
+    left_join(
+      schedule |> 
+        clean_homeaway() |> 
+        select(season, team, week, game_id, opponent, team_score, opponent_score), 
+              by = c("season", "week", "team")
+              )
+  
+  # Step: return stats_df
+  return(stats_df)
 }
 
 off_df <- calculate_adv_stats(posteam, "offense", team_score)
 def_df <- calculate_adv_stats(defteam, "defense", opponent_score)
 
 #--------------------------
-# Bind offense and defense
+# Export
 #--------------------------
 
-bind_rows(off_df, def_df) |> write.csv("Data/off_def_adv_stats.csv")
+bind_rows(off_df, def_df) |> 
+  mutate(join_key = paste(season, week, team, type, sep = '-')) -> off_def_stats
+
+dim_team <- off_def_stats |> select(season:type, join_key) |> 
+  inner_join(teams_colors_logos, by = c("team" = "team_abbr"))
+
+off_def_stats |> write.csv("Data/off_def_adv_stats.csv")
+dim_team |> write.csv("Data/dim_team.csv")
+
 rm(off_df, def_df)
-
-
 
